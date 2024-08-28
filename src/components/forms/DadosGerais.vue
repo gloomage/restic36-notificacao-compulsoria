ea6eca824dfa8dfdab707e7361d2f8ac62552101
gloomage <carlos.mnsp@gmail.com>
2024-08-27 23:22:07 -0300

    feat(components): start of creating forms

---
+++ src/components/forms/DadosGerais.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+    <div class="row q-col-gutter-md q-pl-md">
+      <div class="col-12">
+        <q-input v-model="text" label="Tipo de Notificação" />
+      </div>
+      <div class="col-8">
+        <q-input v-model="text" label="Agravo/doença" />
+      </div>
+      <div class="col-4">
+        <q-input v-model="text" label="Data da Notificação" />
+      </div>
+    </div>
+  </q-form>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  data() {
+    return {
+      text: "",
+    };
+  },
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row">
+      <div class="col-12">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey q-pa-md"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="dados-gerais" label="Dados Gerais" />
+          <q-tab name="notificacao-individual" label="Notificação Individual" />
+          <q-tab name="dados-residencia" label="Dados da Residência" />
+        </q-tabs>
+      </div>
+      <div class="col-12">
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="dados-gerais">
+            <dados-gerais />
+          </q-tab-panel>
+
+          <q-tab-panel name="notificacao-individual"> </q-tab-panel>
+
+          <q-tab-panel name="dados-residencia"> </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
+<script>
+import { defineComponent, ref } from "vue";
+import DadosGerais from "components/forms/DadosGerais.vue";
+
+export default defineComponent({
+  components: {
+    DadosGerais,
+  },
+  data() {
+    return {
+      tab: ref("mails"),
+    };
+  },
 });
 </script>
