b05aa24dc8a746aa1e3816b40c5ae83793520601
gloomage <carlos.mnsp@gmail.com>
2024-11-09 01:08:33 -0300

    style: adicionando pagina de manutenção

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,32 @@
 <template>
   <q-page>
-    <div class="row">
-      <div class="col-12">
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey q-pa-md"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="dados-gerais" label="Dados Gerais" />
-          <q-tab name="notificacao-individual" label="Notificação Individual" />
-          <q-tab name="dados-residencia" label="Dados da Residência" />
-        </q-tabs>
-      </div>
-      <div class="col-12">
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="dados-gerais">
-            <dados-gerais />
-          </q-tab-panel>
-
-          <q-tab-panel name="notificacao-individual"> </q-tab-panel>
-
-          <q-tab-panel name="dados-residencia"> </q-tab-panel>
-        </q-tab-panels>
+    <div
+      class="row"
+      style="
+        height: calc(100cqb - 150px);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      "
+    >
+      <q-img style="max-width: 300px" :src="imageSrc" />
+      <div class="text-h3" style="opacity: 0.4; text-align: center">
+        Página em construção...
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import DadosGerais from "components/forms/DadosGerais.vue";
+import { defineComponent } from "vue";
+import imgSrc from "assets/undraw_innovative_re_rr5i.svg"; // Importa a imagem diretamente
 
 export default defineComponent({
-  components: {
-    DadosGerais,
-  },
   data() {
     return {
-      tab: ref("mails"),
+      imageSrc: imgSrc, // Define a imagem para o src
     };
   },
 });
