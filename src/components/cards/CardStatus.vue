ad1d7a8a39c480d36ad72768ac0dce5ae9848720
gloomage <carlos.mnsp@gmail.com>
2024-11-24 19:52:53 -0300

    feat: adicionando pagina de login

---
+++ src/components/cards/CardStatus.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-card class="card-stats">
+    <div class="card-body">
+      <div class="row">
+        <div class="col">
+          <h5 class="text-uppercase text-muted q-mb-sm">
+            {{ title }}
+          </h5>
+          <span class="h2 font-weight-bold">
+            {{ amount }}
+          </span>
+        </div>
+        <div class="col-auto">
+          <div class="card-circle">
+            <q-icon :name="icon" size="30px" color="white" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardStatus",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    percentage: {
+      type: Number,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.card-stats {
+  min-height: 204px;
+  min-width: 240px;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  border-radius: 0.375rem;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+
+.card-stats .card-body {
+  padding: 1rem 1.5rem;
+}
+
+.card-stats .card-body .card-title {
+  margin-bottom: 1.25rem;
+}
+
+.card-stats .card-body .card-circle {
+  background: linear-gradient(87deg, $primary, $blue-5);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 55px;
+  width: 55px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,32 @@
 <template>
   <q-page>
-    <div
-      class="row"
-      style="
-        height: calc(100cqb - 150px);
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      "
-    >
-      <q-img style="max-width: 300px" :src="imageSrc" />
-      <div class="text-h3" style="opacity: 0.4; text-align: center">
-        Página em construção...
+    <div class="row q-col-gutter-lg q-mb-lg q-pa-lg">
+      <div class="col-4">
+        <card-status
+          class="q-mb-md"
+          title="Notificações Realizadas"
+          amount="104=7"
+          percentage="9"
+          icon="fa-regular fa-handshake"
+        />
+      </div>
+      <div class="col-4">
+        <card-status
+          class="q-mb-md"
+          title="Notificações Enviadas"
+          amount="83"
+          percentage="13"
+          icon="fa-regular fa-user"
+        />
+      </div>
+      <div class="col-4">
+        <card-status
+          class="q-mb-md"
+          title="Notificações Arquivadas"
+          amount="42"
+          percentage="9"
+          icon="fa-regular fa-handshake"
+        />
       </div>
     </div>
   </q-page>
@@ -21,13 +34,17 @@
 
 <script>
 import { defineComponent } from "vue";
-import imgSrc from "assets/undraw_innovative_re_rr5i.svg"; // Importa a imagem diretamente
+import imgSrc from "assets/undraw_innovative_re_rr5i.svg";
+import CardStatus from "components/cards/CardStatus.vue";
 
 export default defineComponent({
   data() {
     return {
-      imageSrc: imgSrc, // Define a imagem para o src
+      imageSrc: imgSrc,
     };
+  },
+  components: {
+    CardStatus,
   },
 });
 </script>
